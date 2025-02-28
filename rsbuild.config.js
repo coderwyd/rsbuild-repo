3d2966058c4094efebbdcd54a50902d352aa1844
donny <donny526@outlook.com>
2025-02-28 14:30:35 +0800

    chore: Add repo

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,9 +1,24 @@
 import { defineConfig } from '@rsbuild/core'
 import { pluginVue2 } from '@rsbuild/plugin-vue2'
+import DefineOptions from 'unplugin-vue-define-options/rspack'
+
 
 export default defineConfig({
-  plugins: [pluginVue2()],
+  plugins: [pluginVue2({
+    vueLoaderOptions:{
+      compilerOptions: {
+        preserveWhitespace: true
+      }
+    }
+  }), ],
   html: {
     template: 'index.html',
-  },
+  }, 
+  tools: {
+    rspack: {
+      plugins: [
+        DefineOptions()
+      ]
+    }
+  }
 })
---
+++ src/demo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Demo</div>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'Demo',
+})
+</script>
+
+<style scoped lang="scss"></style>
